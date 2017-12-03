bd7e0f6d2623f0713159da2742eb32c1018cfb6f
Chefbig <chefbig@gmail.com>
2017-12-03 17:57:50 -0600

    add fragments for deal, order, shopping, shipping

---
+++ app/src/main/java/ca/ncai/finalapp/FragmentShipping.java
@@ -0,0 +1,25 @@
+package ca.ncai.finalapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by niang on 12/3/2017.
+ */
+
+public class FragmentShipping extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_deal, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
