214f73555807894ebe5681aa6e3d24c3b272776c
Chefbig <chefbig@gmail.com>
2017-12-03 22:58:29 -0600

    PostLearn

---
+++ app/src/main/java/ca/ncai/finalapp/models/Comment.java
@@ -0,0 +1,21 @@
+package ca.ncai.finalapp.models;
+
+/**
+ * Created by niang on 12/3/2017.
+ */
+
+public class Comment {
+    public String uid;
+    public String author;
+    public String text;
+
+    public Comment() {
+        // Default constructor required for calls to DataSnapshot.getValue(Comment.class)
+    }
+
+    public Comment(String uid, String author, String text) {
+        this.uid = uid;
+        this.author = author;
+        this.text = text;
+    }
+}
