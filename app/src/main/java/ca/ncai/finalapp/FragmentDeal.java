0e42d5c9292e10ab0355d05ae36c1a9e4deaaf1e
Chefbig <chefbig@gmail.com>
2017-12-04 02:31:08 -0600

    data fetched from firebase but not shown

--- app/src/main/java/ca/ncai/finalapp/FragmentDeal.java
+++ app/src/main/java/ca/ncai/finalapp/FragmentDeal.java
@@ -1,32 +1,24 @@
 package ca.ncai.finalapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import android.util.Log;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.volley.toolbox.NetworkImageView;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import ca.ncai.finalapp.models.Deal;
@@ -37,132 +29,100 @@ import ca.ncai.finalapp.viewholder.DealViewHolder;
  */
 
 public class FragmentDeal extends Fragment {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    FirebaseDatabase database;
-    DatabaseReference dealRef;
-    DealAdapter adapter;
-    private RecyclerView mRecycler;
-    private LinearLayoutManager mManager;
-    private FirebaseRecyclerAdapter<Deal, DealViewHolder> mAdapter;
+    ArrayList<Deal> deals= new ArrayList<>();
+    DatabaseReference myDatabaseRef;
+    private DealAdapter adapter;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        database = FirebaseDatabase.getInstance();
-        dealRef = database.getReference();
-        View rootView = inflater.inflate(R.layout.fragment_deal, container, false);
+        Log.d("Nico", "this onCreateView");
+        //myDatabaseRef = FirebaseDatabase.getInstance().getReference().child("deals");
 
-        mRecycler = rootView.findViewById(R.id.deal_list);
-        mRecycler.setHasFixedSize(true);
+        //myFirebase = new Firebase("https://database-f2992.firebaseio.com/products");
+        myDatabaseRef = FirebaseDatabase.getInstance().getReference().child("deals");
 
-        return rootView;
-    }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        mManager = new LinearLayoutManager(getActivity());
-        mManager.setReverseLayout(true);
-        mManager.setStackFromEnd(true);
-        mRecycler.setLayoutManager(mManager);
-
-        Query postsQuery = getQuery(dealRef);
-
-        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<Deal>()
-                .setQuery(postsQuery, Deal.class)
-                .build();
-        mAdapter = new FirebaseRecyclerAdapter<Deal, DealViewHolder>(options){
-
+        myDatabaseRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public DealViewHolder onCreateViewHolder(View viewGroup, int viewType) {
-                LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
-                return new DealViewHolder(inflater.inflate(R.layout.deal_entry, viewGroup, false));
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("Nico", "this onDataChange");
+                for (DataSnapshot dealSnapshot : dataSnapshot.getChildren()) {
+                    Deal d = dealSnapshot.getValue(Deal.class);
+                    deals.add(d);
+                }
             }
 
             @Override
-            protected void onBindViewHolder(DealViewHolder holder, int position, Deal model) {
-                final DatabaseReference dealRef = getRef(position);
-                final String dealKey = dealRef.getKey();
-                holder.itemView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // Launch PostDetailActivity
-                        Intent intent = new Intent(getActivity(), DealDetailActivity.class);
-                        intent.putExtra(DealDetailActivity.EXTRA_DEAL_KEY, dealKey);
-                        startActivity(intent);
-                    }
-                });
-
+            public void onCancelled(DatabaseError databaseError) {
 
             }
-        };
-        mRecycler.setAdapter(mAdapter);
+        });
+
+//        ValueEventListener dealListener = new ValueEventListener() {
+//
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//                Log.d("Nico", "this onDataChange");
+//                for (DataSnapshot dealSnapshot : dataSnapshot.getChildren()) {
+//                    Deal d = dealSnapshot.getValue(Deal.class);
+//                    deals.add(d);
+//                }
+//            }
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        };
+//
+//        myDatabaseRef.addValueEventListener(dealListener);
+
+        return inflater.inflate(R.layout.fragment_deal, container, false);
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        if(mAdapter != null){
-            mAdapter.stopListening();
+    private Deal getGoodDeal(ArrayList<Deal> deals) {
+        Log.d("Nico", "this getGoodDeal");
+        if(deals.size() !=0){
+            return deals.get((int) Math.random()*deals.size());
         }
-    }
-
-    public String getUid() {
-        return FirebaseAuth.getInstance().getCurrentUser().getUid();
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-//        Toolbar appBar = getActivity().findViewById(R.id.app_bar);
-//        getActivity().setActionBar(appBar);
-        ImageRequester imageRequester = ImageRequester.getInstance(getActivity());
-        Deal headerDeal = getHeaderDeal(dealsArrayList);
-        NetworkImageView headerImage = (NetworkImageView) getActivity().findViewById(R.id.app_bar_image);
-        imageRequester.setImageFromUrl(headerImage, headerDeal.url);
-
-        final RecyclerView recyclerView = (RecyclerView) getActivity().findViewById(R.id.deal_list);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2)); //SharePreferences
-        adapter = new DealAdapter(dealsArrayList, imageRequester);
-        recyclerView.setAdapter(adapter);
-
-        GridLayoutManager layoutManager =
-                (GridLayoutManager) recyclerView.getLayoutManager();
-        layoutManager.scrollToPositionWithOffset(0, 0);
-        getDeals();
-
-    }
-
-    public Query getQuery(DatabaseReference databaseReference)
-    {
-
+        else
+        {
+           throw new IllegalArgumentException("No deal exception");
+        }
     }
 
     @Override
     public void onStart() {
         super.onStart();
-
-        if(mAdapter != null){
-            mAdapter.startListening();
-        }
-    }
+        Log.d("Nico", "this on start");
 
 
-    private Deal getHeaderDeal(ArrayList<Deal> dealsArrayList) {
-        if(dealsArrayList.size() ==0)
-        {
-            throw new IllegalArgumentException("No deals aviliable exception.");
-        }
-        return dealsArrayList.get((int)(Math.random() * dealsArrayList.size()));
+       ImageRequester imageRequester = ImageRequester.getInstance(getActivity());
+
+        final RecyclerView recyclerView = (RecyclerView) getActivity().findViewById(R.id.deal_list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(
+                new GridLayoutManager(getActivity(), getResources().getInteger(R.integer.column_count)));
+        adapter = new DealAdapter(deals, imageRequester);
+        recyclerView.setAdapter(adapter);
+        Log.d("Nico", "checking deals");
+//
+//        Deal goodDeal = getGoodDeal(deals);
+//        NetworkImageView headerImage = (NetworkImageView) getActivity().findViewById(R.id.app_bar_image);
+//        imageRequester.setImageFromUrl(headerImage, goodDeal.url);
+
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+        Log.d("Nico", "this onCreate");
+        super.onCreate(savedInstanceState);
+//        Toolbar appBar = (Toolbar) getActivity().findViewById(R.id.app_bar);
+//        getContext().setSupportActionBar(appBar);
 
     }
 
@@ -171,22 +131,30 @@ public class FragmentDeal extends Fragment {
         private final ImageRequester imageRequester;
 
         DealAdapter(List<Deal> deals, ImageRequester imageRequester) {
+
+            Log.d("Nico", "this DealAdapter");
             this.deals = deals;
             this.imageRequester = imageRequester;
         }
 
         void setDeals(List<Deal> deals) {
+
+            Log.d("Nico", "this setDeals");
             this.deals = deals;
             notifyDataSetChanged();
         }
 
         @Override
         public DealViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+
+            Log.d("Nico", "this onCreateViewHolder");
             return new DealViewHolder(viewGroup);
         }
 
         @Override
         public void onBindViewHolder(DealViewHolder viewHolder, int i) {
+
+            Log.d("Nico", "this onBindViewHolder");
             viewHolder.bind(deals.get(i), imageRequester);
         }
 
@@ -194,5 +162,12 @@ public class FragmentDeal extends Fragment {
         public int getItemCount() {
             return deals.size();
         }
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        Log.d("Nico", "this onViewCreated");
+        super.onViewCreated(view, savedInstanceState);
     }
 }
--- app/src/main/java/ca/ncai/finalapp/models/Deal.java
+++ app/src/main/java/ca/ncai/finalapp/models/Deal.java
@@ -4,6 +4,17 @@ package ca.ncai.finalapp.models;
  * Created by niang on 12/3/2017.
  */
 
-public class Deal extends Product {
-    public Deal(){super();}
+public class Deal{
+    public String price;
+    public String description;
+    public String title;
+    public String url;
+
+    public Deal(){}
+    public Deal(String title, String url, String price, String description) {
+        this.title = title;
+        this.url = url;
+        this.price = price;
+        this.description = description;
+    }
 }
